abae000bbbbe4bda64ddd1e9a4cf50c271364024
Mehr <you@example.com>
2019-07-23 19:49:25 +0430

    fix some bugs and plot

--- Direct_search_based_example.m
+++ Direct_search_based_example.m
@@ -10,18 +10,20 @@ close all
 lb = -40;   %lower band
 ub = 80;    %uper band
 
-% x = lb:ub;
+xx = lb:ub;
 y = @(x) x.^4 - 50*x.^3 - 1800*x.^2 - 800*x;
 % y is handle function (x is functionname)
+plot(xx, y(xx))
+grid on
 
 n  = 1;
-x0 = (ub-lb) * rand(1, n) - lb;
+x0 = (ub-lb) * rand(1, n) + lb;
 y0 = y(x0);
 
 %% Searching algorithm
 maxItr = 10;
 for i=1:maxItr
-    xnew = (ub-lb) * rand(1, n) - lb;
+    xnew = (ub-lb) * rand(1, n) + lb;
     ynew = y(xnew);
     if ynew<y0
         x0 = xnew;
