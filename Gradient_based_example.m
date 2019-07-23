7d50360fe8b14d979d03b2f80009a75563b9b9b8
Mehr <you@example.com>
2019-07-23 17:35:24 +0430

    plot optimum points on the chart

--- Gradient_based_example.m
+++ Gradient_based_example.m
@@ -13,7 +13,16 @@ ub = 80;    %uper band
 % x = lb:ub;
 syms x
 y = x.^4 - 50*x.^3 - 1800*x.^2 - 800*x;
-plot(x,y)
-grid on
 yprim = diff(y,x);
-xopt = vpasolve(yprime==0, x);
+xopt = vpasolve(yprim==0, x);
+
+%% plot the function
+fplot(y,[lb ub])
+grid on
+
+%% plot optimum places of the function
+for i = 1:length(xopt)
+    yopt(i) = subs(y,x,xopt(i));
+    hold on
+    plot(xopt(i), yopt(i), 'o')
+end
