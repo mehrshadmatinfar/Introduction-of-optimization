d4de165090b16899e6422355d50b1f7ef0b0d865
Mehr <you@example.com>
2019-07-23 19:11:45 +0430

    define y as handle function

--- Direct_search_based_example.m
+++ Direct_search_based_example.m
@@ -5,3 +5,11 @@
 clc
 clear
 close all
+
+%% Introduc a function
+lb = -40;   %lower band
+ub = 80;    %uper band
+
+% x = lb:ub;
+y = @(x) x.^4 - 50*x.^3 - 1800*x.^2 - 800*x;
+% y is handle function (x is functionname)
